b5cd8774e195c6da8e800e8de82fed994ef51521
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-09-12 19:46:01 -0300

    chore: Connect component with Unform

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react'
+import { useField } from '@unform/core';
 
-const Input: React.FC = () => {
-  return <h1>Input UnformComponent</h1>
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string
+}
+
+const Input: React.FC<InputProps> = ({ name, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const { fieldName, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+
+  return <input ref={inputRef} type="text" {...rest} />
 }
 
 export default Input
