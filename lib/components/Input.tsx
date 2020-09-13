3546ed3726e43b2ac08b510a8f1cd67aaebf525d
Elias Gabriel <eliasgabrielcf@gmail.com>
2020-09-12 22:57:49 -0300

    chore: Move src to lib

--- lib/components/Input.tsx
+++ lib/components/Input.tsx
@@ -1,7 +1,7 @@
 import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import UnformPropsDTO from './dtos/UnformPropsDTO';
 
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   unformProps: UnformPropsDTO;
 }
